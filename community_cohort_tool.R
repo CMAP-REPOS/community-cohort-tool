764e001f65221cfcefc0a9be61cdbbb2db5883fc
amcadamsCMAP <amcadams@cmap.illinois.gov>
2024-11-04 16:12:17 -0600

    remove EDA from WEIGHTS tab, remove weight interval

--- community_cohort_tool.R
+++ community_cohort_tool.R
@@ -47,9 +47,6 @@ WEIGHTS <- WEIGHTS %>%
     CUT10 = Inf
   )
 
-# Force equal intervals and midpoint of 0.5 for PCT_EDA_POP factor
-WEIGHTS[WEIGHTS$FACTOR_NAME=="PCT_EDA_POP", paste0("CUT", 1:9)] <- as.list(seq(0.1, 0.9, 0.1))
-
 
 ############## PART 2: CALCULATE SCORES
 
