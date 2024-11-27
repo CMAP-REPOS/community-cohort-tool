67ce4da95d3b83377fb449f73a0d00c7ee87809e
Alexis McAdams <amcadams@cmap.illinois.gov>
2024-11-27 09:59:46 -0600

    subset the columns of input datasets

--- pop_in_DAs_R.R
+++ pop_in_DAs_R.R
@@ -17,7 +17,7 @@ library(terra)
 DAs <- st_read(dsn = "V:/Policy/ONTO2050/Layers/LocalStrategyMaps.gdb",
         layer = "Disinvested_and_EDA") %>%
   filter(DA_EDAcomb == "Disinvested" | DA_EDAcomb == "Both")
-  
+
 # import 2020 Census blocks
 cen20blocks <- st_read(dsn = "V:/Demographic_and_Forecast/Census/2020",
                        layer = "dhc_blocks_2020")
@@ -27,6 +27,17 @@ munis <- st_read(dsn = "V:/Administrative_and_Political_Boundaries/Municipalitie
                  layer = "MunisDissolved_2020") %>%
   filter(REGION == 1)
 
+#--------------------------
+
+DAs2 <- DAs %>% select(DA_EDAcomb)
+DAs2$area <- st_area(DAs2)
+DAs2$proportion <- 100
+
+cen20blocks2 <- cen20blocks %>% select(GEOID, POP2020) %>% st_transform(st_crs(DAs2))
+
+munis2 <- munis %>% select(MUNI, FIRST_MUNI_FIPS)
+
+test <- st_interpolate_aw(DAs2 %>% select(-DA_EDAcomb, -area), cen20blocks2, extensive = TRUE)
 
 
 #quickplot of a layer
