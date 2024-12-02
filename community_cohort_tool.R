434a1b56d646b9454973cdf949f8326097109d81
Alexis McAdams <amcadams@cmap.illinois.gov>
2024-12-02 13:29:58 -0600

    adjust variable name

--- community_cohort_tool.R
+++ community_cohort_tool.R
@@ -60,7 +60,7 @@ WEIGHTS <- WEIGHTS %>%
   )
 
 # Force equal intervals and midpoint of 0.5 for PCT_EDA_POP factor
-WEIGHTS[WEIGHTS$FACTOR_NAME=="PCT_EDA_POP", paste0("CUT", 1:9)] <- as.list(seq(0.1, 0.9, 0.1))
+WEIGHTS[WEIGHTS$FACTOR_NAME=="PCT_DA_POP", paste0("CUT", 1:9)] <- as.list(seq(0.1, 0.9, 0.1))
 
 
 ############## PART 2: CALCULATE SCORES
